6580a4207d3083fc26438de2626fa12f1589fe77
ImGabreuw <imgabreuw.dev@gmail.com>
2023-04-15 18:37:59 -0300

    feat: logic to add time

--- app/src/main/java/br/com/alimentadao/app/MainActivity.java
+++ app/src/main/java/br/com/alimentadao/app/MainActivity.java
@@ -1,27 +1,87 @@
 package br.com.alimentadao.app;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements TimeAdapter.OnTimeRemoveListener {
+
+    private List<String> timeCache;
+    private TimeAdapter timeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        RecyclerView recyclerViewTimes = findViewById(R.id.recyclerViewTimes);
+        recyclerViewTimes.setLayoutManager(new LinearLayoutManager(this));
+
+        timeCache = new ArrayList<>();
+        timeAdapter = new TimeAdapter(timeCache, this);
+        recyclerViewTimes.setAdapter(timeAdapter);
+
+        Button buttonAddTime = findViewById(R.id.material_icon_button);
+        buttonAddTime.setOnClickListener(view -> showDialogToAddTime());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        return true;
+    public void onTimeRemoved(int position) {
+        try {
+            Log.i("TAG", "onTimeRemoved: "+ position + "/" + timeCache.toString());
+            timeCache.remove(position);
+            Log.i("TAG", "onTimeRemoved: "+ position + "/" + timeCache.toString());
+            timeAdapter.notifyItemRemoved(position);
+        } catch (IndexOutOfBoundsException ignored) {
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+    private void showDialogToAddTime() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_add_time, null);
+        EditText etTime = dialogView.findViewById(R.id.etTime);
+
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "Adicionar", (dialog1, which) -> {
+            String time = etTime.getText().toString();
+
+            if (time.trim().length() == 0) {
+                Toast.makeText(
+                        getApplicationContext(),
+                        "Insira um horário válido (HH:MM)",
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+
+            timeCache.add(time);
+            timeAdapter.notifyItemInserted(timeCache.size() - 1);
+            dialog1.dismiss();
+
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Horário adicionar com sucesso",
+                    Toast.LENGTH_SHORT
+            ).show();
+        });
+
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancelar", (dialog12, which) -> dialog12.dismiss());
+
+        dialog.show();
     }
 }
---
+++ app/src/main/java/br/com/alimentadao/app/TimeAdapter.java
@@ -0,0 +1,68 @@
+package br.com.alimentadao.app;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TimeAdapter extends RecyclerView.Adapter<TimeAdapter.TimeViewHolder> {
+
+    private final List<String> timeCache;
+    private final OnTimeRemoveListener onTimeRemoveListener;
+
+    public TimeAdapter(List<String> timeCache, OnTimeRemoveListener onTimeRemoveListener) {
+        this.timeCache = timeCache;
+        this.onTimeRemoveListener = onTimeRemoveListener;
+    }
+
+    @NonNull
+    @Override
+    public TimeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(
+                        R.layout.list_item_time,
+                        parent,
+                        false
+                );
+
+        return new TimeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TimeViewHolder holder, int position) {
+        String time = timeCache.get(position);
+
+        holder.textViewTime.setText(time);
+        holder.buttonRemove.setOnClickListener(view -> onTimeRemoveListener.onTimeRemoved(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return timeCache.size();
+    }
+
+    static class TimeViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewTime;
+        ImageButton buttonRemove;
+
+        TimeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTime = itemView.findViewById(R.id.textViewTime);
+            buttonRemove = itemView.findViewById(R.id.buttonRemove);
+        }
+    }
+
+    public interface OnTimeRemoveListener {
+        void onTimeRemoved(int position);
+    }
+}
+
+
+
