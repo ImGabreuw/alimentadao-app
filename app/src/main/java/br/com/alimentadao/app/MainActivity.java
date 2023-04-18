6efd2b3e3cef5af44c688d7a8e1073da4a725e7e
ImGabreuw <imgabreuw.dev@gmail.com>
2023-04-17 21:29:46 -0300

    feat: add button to change profile image

--- app/src/main/java/br/com/alimentadao/app/MainActivity.java
+++ app/src/main/java/br/com/alimentadao/app/MainActivity.java
@@ -1,10 +1,17 @@
 package br.com.alimentadao.app;
 
+import static android.content.Intent.ACTION_PICK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -16,6 +23,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements TimeAdapter.OnTimeRemoveListener {
+
+    private static final int REQUEST_PICK_IMAGE = 1;
 
     private List<String> timeCache;
     private TimeAdapter timeAdapter;
@@ -34,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements TimeAdapter.OnTim
 
         Button buttonAddTime = findViewById(R.id.btnAddTime);
         buttonAddTime.setOnClickListener(view -> showDialogToAddTime());
+
+        ImageButton buttonChangeImgProfile = findViewById(R.id.btnChangeImgProfile);
+        buttonChangeImgProfile.setOnClickListener(view -> openGallery());
     }
 
     @Override
@@ -78,5 +90,30 @@ public class MainActivity extends AppCompatActivity implements TimeAdapter.OnTim
         dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancelar", (dialog12, which) -> dialog12.dismiss());
 
         dialog.show();
+    }
+
+    private void openGallery() {
+        Intent intent = new Intent(ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, REQUEST_PICK_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode != REQUEST_PICK_IMAGE || resultCode != RESULT_OK || data == null) {
+            return;
+        }
+
+        Uri imageUri = data.getData();
+
+        ImageView meuImageButton = findViewById(R.id.dogImage);
+        meuImageButton.setImageURI(imageUri);
+
+        Toast.makeText(
+                getApplicationContext(),
+                "Foto atualizada com sucesso",
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
